7053612dc635fcd292808493ab8051ba84cf1403
Mateusz Mesek <mateusz@mesek.pl>
2022-04-17 01:51:48 +0200

    wip

--- src/IndexerHandler/Adapter.php
+++ src/IndexerHandler/Adapter.php
@@ -12,4 +12,39 @@ class Adapter extends BaseAdapter
 
         return parent::addDocs($documents, $storeId, $mappedIndexerId);
     }
+
+    protected function getDocsArrayInBulkIndexFormat(
+        $documents,
+        $indexName,
+        $action = self::BULK_ACTION_UPDATE
+    ) {
+        $bulkArray = [
+            'index' => $indexName,
+            'type' => $this->clientConfig->getEntityType(),
+            'body' => [],
+            'refresh' => true,
+        ];
+
+        foreach ($documents as $id => $document) {
+            $bulkArray['body'][] = [
+                $action => [
+                    '_id' => $id,
+                    '_type' => $this->clientConfig->getEntityType(),
+                    '_index' => $indexName
+                ]
+            ];
+
+            switch ($action) {
+                case self::BULK_ACTION_INDEX:
+                    $bulkArray['body'][] = $document;
+                    break;
+
+                case self::BULK_ACTION_UPDATE:
+                    $bulkArray['body'][] = ['doc' => $document, 'doc_as_upsert' => true];
+                    break;
+            }
+        }
+
+        return $bulkArray;
+    }
 }
